bbaccc2af8020cfb72a6d222a51b99234af5996a
kkougios <kostas.kougios@googlemail.com>
2025-08-10 16:50:52 +0100

    -

--- src/main/scala/codeoptimizer/OptimizerPlugin.scala
+++ src/main/scala/codeoptimizer/OptimizerPlugin.scala
@@ -4,9 +4,9 @@ import dotty.tools.dotc.core.Contexts.*
 import dotty.tools.dotc.plugins.*
 
 class OptimizerPlugin extends StandardPlugin:
-  val name: String        = "OptimizerPlugin"
-  val description: String =
-    "Optimizes scala collection operations"
+  override val name: String        = "OptimizerPlugin"
+  override val description: String = "Optimizes scala collection operations"
 
-  override def init(options: List[String]): List[PluginPhase] =
-    List(new FilterForallOptimizer)
+  override def init(options: List[String]): List[PluginPhase] = List(
+    new FilterForallOptimizer
+  )
