db206fc285105aacdf9ddae77b17a42319ab6075
kkougios <kostas.kougios@googlemail.com>
2025-08-15 00:47:33 +0100

    -

--- test-project/src/main/scala/codeoptimizer/SeqOps.scala
+++ test-project/src/main/scala/codeoptimizer/SeqOps.scala
@@ -2,7 +2,17 @@ package codeoptimizer
 
 object SeqOps:
   def filterMap[A, B](pred: A => Boolean, mapper: A => B, xs: Seq[A]): Seq[B] =
-    xs.iterator.filter(pred).map(mapper).toSeq
+    xs match
+      case l: List[A] =>
+        if l.isEmpty then Nil
+        else
+          val b      = List.newBuilder[B]
+          var remain = l
+          while !remain.isEmpty do
+            if pred(remain.head) then b += mapper(remain.head)
+            remain = remain.tail
+
+          b.result()
 
   def filterForall[A](pred: A => Boolean, all: A => Boolean, xs: Seq[A]): Boolean =
     xs.iterator.filter(pred).forall(all)
