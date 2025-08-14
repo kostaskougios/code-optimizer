a74f51ce60277e32ffe96dfceb158b6686887d8a
kkougios <kostas.kougios@googlemail.com>
2025-08-15 00:56:41 +0100

    -

--- test-project/src/main/scala/benchmark.scala
+++ test-project/src/main/scala/benchmark.scala
@@ -1,10 +1,23 @@
 import game.utils.Timings
+
+/**
+   * Without plugin:
+┌──────────────────┬───────┬───────┬───────────┬───────┬───────────────┐
+│ Name             │ Last  │ Min   │ Total     │ Calls │ Per Call (ns) │
+├──────────────────┼───────┼───────┼───────────┼───────┼───────────────┤
+│ filter-map-large │ 3.169 │ 2.847 │ 8.663.708 │ 2.699 │ 3209969       │
+│ filter-map-small │ 3.364 │ 2.725 │ 9.127.688 │ 2.699 │ 3381877       │
+└──────────────────┴───────┴───────┴───────────┴───────┴───────────────┘
+  */
+
 @main
 def benchmark(): Unit =
   var i = 0
   while true do
-    Timings.profile("filter-map"):
+    Timings.profile("filter-map-large"):
       lists.ListsBenchmark.runFilterMap()
+    Timings.profile("filter-map-small"):
+      lists.ListsBenchmark.runFilterMapSmallList()
 
     i += 1
     if i % 100 == 99 then println(Timings.profilingTable)
--- test-project/src/main/scala/lists/ListsBenchmark.scala
+++ test-project/src/main/scala/lists/ListsBenchmark.scala
@@ -1,20 +1,21 @@
 package lists
 
 object ListsBenchmark:
-  private val l = (1 to 1000).toList
+  private val largeList = (1 to 1000).toList
+  private val smallList = List(1, 2, 3)
 
-  /**
-   * Without plugin:
-┌────────────┬───────┬───────┬────────────┬────────┬───────────────┐
-│ Name       │ Last  │ Min   │ Total      │ Calls  │ Per Call (ns) │
-├────────────┼───────┼───────┼────────────┼────────┼───────────────┤
-│ filter-map │ 3.238 │ 2.879 │ 39.499.193 │ 12.299 │ 3211577       │
-└────────────┴───────┴───────┴────────────┴────────┴───────────────┘
-   */
   def runFilterMap(): List[Int] =
     var i = 0
     var r = List.empty[Int]
     while i < 1000 do
-      l.filter(_ > 500).map(_ * 2)
+      r = largeList.filter(_ > 500).map(_ * 2)
+      i += 1
+    r
+
+  def runFilterMapSmallList(): List[Int] =
+    var i = 0
+    var r = List.empty[Int]
+    while i < 300000 do
+      r = smallList.filter(_ >= 2).map(_ * 2)
       i += 1
     r
