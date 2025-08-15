a6a8e918ee85588a49542a15be600826bcdc3f42
kkougios <kostas.kougios@googlemail.com>
2025-08-15 04:05:52 +0100

    -

--- test-project/src/test/scala/codeoptimizer/ListOpsTest.scala
+++ test-project/src/test/scala/codeoptimizer/ListOpsTest.scala
@@ -8,7 +8,7 @@ class ListOpsTest extends AnyFunSuiteLike:
   for i <- 0 to 10 do
     for j <- 0 to i + 1 do
       test(s"filterMap list of $i size and $j filter"):
-        val l    = (0 to i - 1).toList
+        val l    = (0 until i).toList
         val lops = ListOps.filterMap[Int, Int](_ >= j, _ * 2, l)
         val n    = l.filter(_ >= j).map(_ * 2)
         // println(s"$l , $lops , $n")
