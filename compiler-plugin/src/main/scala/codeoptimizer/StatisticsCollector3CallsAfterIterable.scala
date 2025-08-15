e64155f65c4f6a7f97730c7ab8ae91caf9cb1eb8
kkougios <kostas.kougios@googlemail.com>
2025-08-15 23:05:37 +0100

    -

--- compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollector3CallsAfterIterable.scala
+++ compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollector3CallsAfterIterable.scala
@@ -4,14 +4,41 @@ import dotty.tools.dotc.*
 import dotty.tools.dotc.ast.tpd.*
 import dotty.tools.dotc.core.*
 import dotty.tools.dotc.core.Contexts.*
+import dotty.tools.dotc.core.Names.Name
 import dotty.tools.dotc.core.Symbols.*
 import dotty.tools.dotc.core.Types.*
 
 class StatisticsCollector3CallsAfterIterable(using Context) extends AbstractOptimizer:
   private val IterableClass = requiredClass("scala.collection.Iterable").typeRef.appliedTo(TypeBounds.empty)
 
+  private def recordStats(seqExpr: dotty.tools.dotc.ast.Trees.Tree[Type], call1: Name, call2: Name): Unit =
+    val iterableName = seqExpr.tpe.widenTermRefExpr.typeSymbol.name.show
+    Statistics.inc(s"$iterableName.${call1.mangledString}.${call2.mangledString}")
+
   override def transformApply(tree: Apply)(using Context): Apply =
     tree match
+      case Apply(
+            Select(
+              Apply(
+                TypeApply(
+                  Select(
+                    Apply(
+                      Select(seqExpr, call1),
+                      List(call1Param)
+                    ),
+                    call2
+                  ),
+                  List(call2Type)
+                ),
+                List(call2Param)
+              ),
+              call3
+            ),
+            List(call3Param)
+          ) if seqExpr.tpe <:< IterableClass =>
+
+        recordStats(seqExpr, call1, call2)
+        tree
       case Apply(
             Select(
               Apply(
@@ -29,8 +56,7 @@ class StatisticsCollector3CallsAfterIterable(using Context) extends AbstractOpti
             List(call3Param)
           ) if seqExpr.tpe <:< IterableClass =>
 
-        val iterableName = seqExpr.tpe.widenTermRefExpr.typeSymbol.name.show
-        Statistics.inc(s"$iterableName.${call1.mangledString}.${call2.mangledString}.${call3.mangledString}")
+        recordStats(seqExpr, call1, call2)
         tree
       case _ =>
         tree
