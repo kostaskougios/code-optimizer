d4464dd94ab1df0d7a4dd58408271b35d72c661d
kkougios <kostas.kougios@googlemail.com>
2025-08-15 22:47:35 +0100

    -

--- compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollector2CallsAfterIterable.scala
+++ compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollector2CallsAfterIterable.scala
@@ -9,7 +9,6 @@ import dotty.tools.dotc.core.Types.*
 
 class StatisticsCollector2CallsAfterIterable(using Context) extends AbstractOptimizer:
   private val IterableClass = requiredClass("scala.collection.Iterable").typeRef.appliedTo(TypeBounds.empty)
-  private val SeqClass      = defn.SeqClass.typeRef.appliedTo(TypeBounds.empty)
 
   override def transformApply(tree: Apply)(using Context): Apply =
     tree match
@@ -23,6 +22,20 @@ class StatisticsCollector2CallsAfterIterable(using Context) extends AbstractOpti
                 call2
               ),
               List(call2Type)
+            ),
+            List(call2Param)
+          ) if seqExpr.tpe <:< IterableClass =>
+
+        val iterableName = seqExpr.tpe.widenTermRefExpr.typeSymbol.name.show
+        Statistics.inc(s"$iterableName.${call1.mangledString}.${call2.mangledString}")
+        tree
+      case Apply(
+            Select(
+              Apply(
+                Select(seqExpr, call1),
+                List(call1Param)
+              ),
+              call2
             ),
             List(call2Param)
           ) if seqExpr.tpe <:< IterableClass =>
