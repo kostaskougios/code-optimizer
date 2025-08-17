c51624ac9dd936d40a9e00ca3e16376a4494cf34
kkougios <kostas.kougios@googlemail.com>
2025-08-17 15:34:35 +0100

    -

--- compiler-plugin/src/main/scala/codeoptimizer/StringUtils.scala
+++ compiler-plugin/src/main/scala/codeoptimizer/StringUtils.scala
@@ -3,10 +3,6 @@ package codeoptimizer
 import java.text.{DecimalFormat, DecimalFormatSymbols}
 import java.util.Locale
 
-extension (d: Double) def to2DigitDecimalPointString: String = if d.isNaN then "-" else f"$d%.2f"
-
-extension (b: Boolean) def toYesNo: String = if b then "Yes" else "No"
-
 object StringUtils:
   private val symbols = new DecimalFormatSymbols(Locale.UK)
   symbols.setGroupingSeparator('.') // use dot as thousands separator
