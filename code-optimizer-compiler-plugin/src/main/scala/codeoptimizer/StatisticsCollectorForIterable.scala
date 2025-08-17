02cd3668995c85cd5a636e96c023374a1278422f
kkougios <kostas.kougios@googlemail.com>
2025-08-17 16:33:04 +0100

    -

--- code-optimizer-compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollectorForIterable.scala
+++ code-optimizer-compiler-plugin/src/main/scala/codeoptimizer/StatisticsCollectorForIterable.scala
@@ -1,5 +1,6 @@
 package codeoptimizer
 
+import codeoptimizer.Utils.reportMsg
 import dotty.tools.dotc.*
 import dotty.tools.dotc.ast.Trees
 import dotty.tools.dotc.ast.tpd.*
@@ -12,30 +13,33 @@ import dotty.tools.dotc.core.Types.*
 class StatisticsCollectorForIterable(using Context) extends AbstractOptimizer:
   private val IterableClass = requiredClass("scala.collection.Iterable").typeRef.appliedTo(TypeBounds.empty)
 
-  def scanApply(tree: Trees.Tree[Type])(using Context): Option[Rec] =
+  def scanApply(tree: Apply)(using Context): Option[Rec] =
     val calls     = scanSeqApply(tree)
     val seqStarts = calls.dropWhile(c => !(c.selectOn.tpe <:< IterableClass))
     if seqStarts.isEmpty then None
-    else Some(Rec(seqStarts.head.selectOn, seqStarts))
+    else Some(Rec(tree, seqStarts.head.selectOn, seqStarts))
 
-  def scanSeqApply(tree: dotty.tools.dotc.ast.Trees.Tree[Type])(using Context): List[Call] =
+  def scanSeqApply(tree: Trees.Tree[Type])(using Context): List[Call] =
     tree match
       case Apply(Select(app, call), callParams)                       => scanSeqApply(app) :+ Call(app, call, callParams, Nil)
       case Apply(TypeApply(Select(app, call), callTypes), callParams) => scanSeqApply(app) :+ Call(app, call, callParams, callTypes)
       case _                                                          => Nil
 
-  private def recordStats(seqExpr: dotty.tools.dotc.ast.Trees.Tree[Type], calls: List[Call]): Unit =
-    val iterableName = seqExpr.tpe.widenTermRefExpr.typeSymbol.name.show
-    Statistics.inc(s"$iterableName.${calls.map(_.name.mangledString).mkString(".")}")
+  private def recordStats(rec: Rec): Unit =
+    val iterableName = rec.seqExpr.tpe.widenTermRefExpr.typeSymbol.name.show
+    val callsStr     = rec.calls.map(_.name.mangledString).mkString(".")
+    reportMsg(getClass, callsStr, rec.tree)
+    Statistics.inc(s"$iterableName.$callsStr")
 
   override def transformApply(tree: Apply)(using Context): Apply =
     scanApply(tree) match
-      case Some(rec) if rec.calls.size > 1 => recordStats(rec.seqExpr, rec.calls)
+      case Some(rec) if rec.calls.size > 1 => recordStats(rec)
       case _                               =>
     tree
 
 case class Rec(
-    seqExpr: dotty.tools.dotc.ast.Trees.Tree[Type],
+    tree: Apply,
+    seqExpr: Trees.Tree[Type],
     calls: List[Call]
 ):
   def +(call: Call) = copy(calls = calls :+ call)
