943f711a0a490ed5ceee372cbd33eedfde52e166
kkougios <kostas.kougios@googlemail.com>
2025-08-17 15:37:48 +0100

    -

--- test-project/src/test/scala/codeoptimizer/DoesNotCorruptCodeTest.scala
+++ test-project/src/test/scala/codeoptimizer/DoesNotCorruptCodeTest.scala
@@ -9,6 +9,7 @@ import org.scalatest.matchers.should.Matchers.*
 class DoesNotCorruptCodeTest extends AnyFunSuiteLike:
   test("filterMap"):
     (1 to 5).toList.filter(_ > 2).map(_ * 2).toSet should be(Set(3 * 2, 4 * 2, 5 * 2))
+    (1 to 5).filter(_ > 2).map(_ * 2).toSet should be(Set(3 * 2, 4 * 2, 5 * 2))
 
   test("withFilterForeach"):
     var i = 0
@@ -18,3 +19,9 @@ class DoesNotCorruptCodeTest extends AnyFunSuiteLike:
   test("filterForall"):
     (1 to 5).toList.filter(_ > 2).forall(_ > 2) should be(true)
     (1 to 5).toList.filter(_ > 2).forall(_ < 2) should be(false)
+
+    (1 to 5).filter(_ > 2).forall(_ > 2) should be(true)
+    (1 to 5).filter(_ > 2).forall(_ < 2) should be(false)
+
+    Iterable(1, 2, 3, 4, 5).filter(_ > 2).forall(_ > 2) should be(true)
+    Iterable(1, 2, 3, 4, 5).filter(_ > 2).forall(_ < 2) should be(false)
