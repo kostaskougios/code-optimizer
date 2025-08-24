0a5f81e2e1c17c0818573b502b66e5473d8605c6
kkougios <kostas.kougios@googlemail.com>
2025-08-24 17:06:08 +0100

    -

---
+++ code-optimizer-lib/src/test/scala/codeoptimizer/SeqOpsButForListTest.scala
@@ -0,0 +1,10 @@
+package codeoptimizer
+
+class SeqOpsButForListTest extends AbstractIterableTest[Seq[Int]]("Seq(List)"):
+  override def filterMap(l: Seq[Int], pred: Int => Boolean, mapper: Int => Int)           = SeqOps.filterMap(l, pred, mapper)
+  override def mapFilter(l: Seq[Int], mapper: Int => Int, pred: Int => Boolean)           = SeqOps.mapFilter(l, mapper, pred)
+  override def withFilterForeach(l: Seq[Int], pred: Int => Boolean, f: Int => Unit): Unit = SeqOps.withFilterForeach(l, pred, f)
+  override def filterForall(l: Seq[Int], pred: Int => Boolean, all: Int => Boolean)       = SeqOps.filterForall(l, pred, all)
+  override def mapFind(l: Seq[Int], mapper: Int => Int, pred: Int => Boolean)             = SeqOps.mapFind(l, mapper, pred)
+
+  override def createIterable(i: Int): Seq[Int] = (0 until i).toList
