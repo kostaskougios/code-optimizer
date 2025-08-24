9ff6ceea4454eed49e97d74bf85942b6199398a1
kkougios <kostas.kougios@googlemail.com>
2025-08-24 17:05:29 +0100

    -

--- code-optimizer-lib/src/test/scala/codeoptimizer/ListOpsTest.scala
+++ code-optimizer-lib/src/test/scala/codeoptimizer/ListOpsTest.scala
@@ -2,9 +2,9 @@ package codeoptimizer
 
 class ListOpsTest extends AbstractIterableTest[List[Int]]("List"):
   override def filterMap(l: List[Int], pred: Int => Boolean, mapper: Int => Int): List[Int] = ListOps.filterMap(l, pred, mapper)
-  def mapFilter(l: List[Int], mapper: Int => Int, pred: Int => Boolean)                     = ListOps.mapFilter(l, mapper, pred)
-  def withFilterForeach(l: List[Int], pred: Int => Boolean, f: Int => Unit): Unit           = ListOps.withFilterForeach(l, pred, f)
-  def filterForall(l: List[Int], pred: Int => Boolean, all: Int => Boolean)                 = ListOps.filterForall(l, pred, all)
-  def mapFind(l: List[Int], mapper: Int => Int, pred: Int => Boolean)                       = ListOps.mapFind(l, mapper, pred)
+  override def mapFilter(l: List[Int], mapper: Int => Int, pred: Int => Boolean)            = ListOps.mapFilter(l, mapper, pred)
+  override def withFilterForeach(l: List[Int], pred: Int => Boolean, f: Int => Unit): Unit  = ListOps.withFilterForeach(l, pred, f)
+  override def filterForall(l: List[Int], pred: Int => Boolean, all: Int => Boolean)        = ListOps.filterForall(l, pred, all)
+  override def mapFind(l: List[Int], mapper: Int => Int, pred: Int => Boolean)              = ListOps.mapFind(l, mapper, pred)
 
   override def createIterable(i: Int): List[Int] = (0 until i).toList
--- code-optimizer-lib/src/test/scala/codeoptimizer/SeqOpsTest.scala
+++ code-optimizer-lib/src/test/scala/codeoptimizer/SeqOpsTest.scala
@@ -3,35 +3,11 @@ package codeoptimizer
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 
-class SeqOpsTest extends AnyFunSuiteLike:
-  for i <- 0 to 10 do
-    for j <- 0 to i + 1 do
-      test(s"filterMap list of $i size and $j filter"):
-        val l    = (0 until i).toList
-        val lops = SeqOps.filterMap[Int, Int](l, _ >= j, _ * 2)
-        val n    = l.filter(_ >= j).map(_ * 2)
-        lops should be(n)
+class SeqOpsTest extends AbstractIterableTest[Seq[Int]]("Seq"):
+  override def filterMap(l: Seq[Int], pred: Int => Boolean, mapper: Int => Int)           = SeqOps.filterMap(l, pred, mapper)
+  override def mapFilter(l: Seq[Int], mapper: Int => Int, pred: Int => Boolean)           = SeqOps.mapFilter(l, mapper, pred)
+  override def withFilterForeach(l: Seq[Int], pred: Int => Boolean, f: Int => Unit): Unit = SeqOps.withFilterForeach(l, pred, f)
+  override def filterForall(l: Seq[Int], pred: Int => Boolean, all: Int => Boolean)       = SeqOps.filterForall(l, pred, all)
+  override def mapFind(l: Seq[Int], mapper: Int => Int, pred: Int => Boolean)             = SeqOps.mapFind(l, mapper, pred)
 
-      test(s"mapFilter list of $i size and $j filter"):
-        val l    = (0 until i).toList
-        val lops = SeqOps.mapFilter[Int, Int](l, _ * 2, _ >= j * 2)
-        val n    = l.map(_ * 2).filter(_ >= j * 2)
-        lops should be(n)
-
-      test(s"withFilterForeach list of $i size and $j filter"):
-        val l    = (0 until i).toList
-        var sum1 = 0
-        SeqOps.withFilterForeach[Int, Unit](l, _ >= j, sum1 += _)
-        var sum2 = 0
-        l.withFilter(_ >= j).foreach(sum2 += _)
-        sum1 should be(sum2)
-
-      test(s"filterForall list of $i size and $j filter"):
-        val l = (0 until i).toList
-        SeqOps.filterForall[Int](l, _ >= j, _ >= j) should be(true)
-        SeqOps.filterForall[Int](l, _ >= j, _ < 0) should be(!l.exists(_ >= j))
-
-      test(s"mapFind list of $i size and $j find"):
-        val l        = (0 until i).toList
-        val expected = if i == 0 || j >= i then None else Some(j * 2)
-        SeqOps.mapFind(l, _ * 2, _ == j * 2) should be(expected)
+  override def createIterable(i: Int): Seq[Int] = (0 until i).toVector
